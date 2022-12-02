54f63e792dcbc107fdc3db1ed8332e638ee332e4
pro100ton <pro100ton@gmail.com>
2022-12-02 22:38:48 +0300

    Trying to add file handler

--- telegram/bot.py
+++ telegram/bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 from pathlib import Path
+import concurrent.futures
 
 from dotenv import load_dotenv
 from telegram.ext.filters import MessageFilter
@@ -12,6 +13,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Messa
 # Configuring logger
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
 
 dotenv_path = Path(__file__).parent.parent.absolute()
 load_dotenv(dotenv_path=os.path.join(dotenv_path, '.env.dev'))
@@ -25,6 +27,11 @@ logging.basicConfig(
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def rule_parser(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # context.bot.get_file(update.message.document).download()
+    with open("file.json", "wb") as f:
+        await context.bot.get_file(update.message.document).download(out=f)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
@@ -44,5 +51,6 @@ if __name__ == '__main__':
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
+    application.add_handler(MessageHandler(filters.Document.ALL, rule_parser))
     
     application.run_polling()
