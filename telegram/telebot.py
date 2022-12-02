630e1f02f5adf8191b0ab2b6121802b113cb20d3
pro100ton <pro100ton@gmail.com>
2022-12-02 21:36:14 +0300

    Add /start handler

--- telegram/telebot.py
+++ telegram/telebot.py
@@ -5,13 +5,24 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 import telegram
+from telegram.ext import CallbackContext, Updater, CommandHandler
 
 
 # Configuring logger
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
 dotenv_path = Path(__file__).parent.parent.absolute()
 load_dotenv(dotenv_path=os.path.join(dotenv_path, '.env.dev'))
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-bot = telegram.Bot(token=BOT_TOKEN)
-print(bot.get_me())
+updater = Updater(token=BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update: Updater, context: CallbackContext):
+    context.bot.send_message(chat_id = update.effective_chat.id, text="Hello, im bot")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
